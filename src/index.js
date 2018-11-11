f50f6bc688404b87fb3ab66746fd5723c8a7add4
Shreyas Kalyanaraman <shreyas.kalyanaraman@gmail.com>
2018-11-10 23:38:51 -0500

    Adding Middleware - redux saga

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import  React  from 'react';
 import  ReactDOM   from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga' 
+
 import './index.css';
 import App  from './App';
 import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
 import { addUser } from './actions'
+import { setupSocket } from './sockets'
+import handleNewMessage from './sage'
+import username from '.utils/name'
 
-import chat from './reducers'
+import reducers from './reducers'
 
-const store = createStore(chat)
+const sageMiddleware = createSagaMiddleware()
+
+const store = createStore(
+    reducers,
+    applyMiddleware(sageMiddleware)
+)
+
 
 store.dispatch(addUser('Me'))
 
